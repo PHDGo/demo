8ab3d2c79582b56762c31b6b09425551b4b5fe6f
phdgo <hikaso@163.com>
2020-03-02 15:31:21 +0800

    change img

--- swiper/swiper.js
+++ swiper/swiper.js
@@ -1,11 +1,20 @@
 (function() {
-	var swiper = function() {
-
+	var swiper = function(el, urls) {
+		this.el = el
+		this.nodeNames = ['body', 'pagination', 'left', 'right']
+		this.init()
 	};
 	
 	swiper.prototype = {
-		constructor: swiper
+		constructor: swiper,
+		init () {
+			this.nodeNames.forEach((nodeName) => {
+				let node = document.createElement('div'),
+				node.className = nodeName
+				this.el.appendChild(node)
+			})
+		}
 	};
 
-	window.swiper = swiper;
+	window.Swiper = swiper;
 })()
